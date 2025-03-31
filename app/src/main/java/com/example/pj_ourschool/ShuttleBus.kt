a76551242968a3bba868a7f26471d499465eecde
phj01 <phj01040311230@gmail.com>
2025-03-31 22:57:11 +0900

    형주 커밋

---
+++ app/src/main/java/com/example/pj_ourschool/ShuttleBus.kt
@@ -0,0 +1,11 @@
+package com.example.pj_ourschool
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShuttleBus : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shuttlebus) // 시간표 레이아웃 설정
+    }
+}
---
+++ app/src/main/java/com/example/pj_ourschool/Time.kt
@@ -0,0 +1,11 @@
+package com.example.pj_ourschool
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Time : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_time_table) // 시간표 레이아웃 설정
+    }
+}
