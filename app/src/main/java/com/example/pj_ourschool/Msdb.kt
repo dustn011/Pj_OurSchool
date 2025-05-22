2875712b68836334e47c9f1d6aa890bab72e3f58
phj01 <phj01040311230@gmail.com>
2025-05-22 17:52:08 +0900

    셔틀 버스 ui 구성

--- app/src/main/java/com/example/pj_ourschool/Msdb.kt
+++ app/src/main/java/com/example/pj_ourschool/Msdb.kt
@@ -7,7 +7,7 @@ import java.sql.DriverManager
 
 
 object MSSQLConnector {
-    private const val URL = "jdbc:jtds:sqlserver://210.124.50.238;databaseName=DB"
+    private const val URL = "jdbc:jtds:sqlserver://112.72.143.120;databaseName=DB"
     private const val USER = "엉구"
     private const val PASSWORD = "1234"
 
